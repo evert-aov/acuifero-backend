2dd3cf5446d6d5c1b5a1b29baecfd301cab50358
evert.aov <evert.aov@gmail.com>
2026-05-30 22:44:30 -0400

    Elimina el archivo `__init__.py` innecesario en `app/controllers`

--- app/controllers/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.controllers import municipio_controller, alerta_controller, prediccion_controller, gemini_controller
-
-__all__ = ["municipio_controller", "alerta_controller", "prediccion_controller", "gemini_controller"]
